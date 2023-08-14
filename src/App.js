1f6220003f086f029dcacf6dea69c52bcc504e24
Braden <braden.clokie@gmail.com>
2023-08-14 12:48:18 -0400

    Added home page code, working on game pages now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import GameDropdown from './components/gameDropdown';
-import SearchBar from './components/searchBar';
-import HomePage from './pages/homePage';
+import HomePage from './pages/homePage'; 
 import GamePage from './pages/gamePage';
-import { fetchGameTitles } from './helpers/api'; 
-import { toggleDarkMode } from './helpers/darkmodeUtil'; 
-import { BrowserRouter as Router, Link } from 'react-router-dom'; // Import BrowserRouter and Link from react-router-dom
+import { fetchGameTitles } from './helpers/api';
+import { toggleDarkMode } from './helpers/darkmodeUtil';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -14,7 +12,6 @@ const App = () => {
   const [selectedGame, setSelectedGame] = useState(null);
 
   useEffect(() => {
-    // Fetch game titles using the API helper
     fetchGameTitles()
       .then((fullcrossesTitles) => {
         setGameTitles(fullcrossesTitles);
@@ -25,40 +22,31 @@ const App = () => {
   }, []);
 
   const handleSelectGame = (selectedGame) => {
-    console.log('Selected game:', selectedGame);
     setSelectedGame(selectedGame);
   };
-
+  
   const handleToggleDarkMode = () => {
-    // Toggle dark mode using the dark mode helper
     toggleDarkMode(darkMode, setDarkMode);
   };
 
   return (
-    <Router> {/* Wrap your App component with BrowserRouter */}
-      <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
-        <nav className="navbar">
-          <h1>CrossConnect</h1>
-          <div className="search-bar">
-            <SearchBar onSelectGame={handleSelectGame} gameTitles={gameTitles} darkMode={darkMode} />
-          </div>
-          <button className="dark-mode-button" onClick={handleToggleDarkMode}>
-            {darkMode ? 'Light Mode' : 'Dark Mode'}
-          </button>
-        </nav>
-        <div className="App">
-          <h1>Game Selection</h1>
-          <GameDropdown onSelectGame={handleSelectGame} selectedGame={selectedGame} />
-          {selectedGame && (
-            <div>
-              <button className="game-page-button" disabled={!selectedGame}>
-                <Link to={`/games/${encodeURIComponent(selectedGame)}`}>Go to Game Page</Link>
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-    </Router> // {/* Close the BrowserRouter */}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              darkMode={darkMode}
+              handleSelectGame={handleSelectGame}
+              gameTitles={gameTitles}
+              selectedGame={selectedGame}
+              handleToggleDarkMode={handleToggleDarkMode}
+            />
+          }
+        />
+        <Route path="/games/:gameName" element={<GamePage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import GameDropdown from '../components/gameDropdown'; 
+import GameDropdown from '../components/gameDropdown';
+import SearchBar from '../components/searchBar';
+import { Link } from 'react-router-dom';
 
-const HomePage = ({ gameTitles }) => {
+const HomePage = ({ darkMode, handleSelectGame, gameTitles, selectedGame, handleToggleDarkMode }) => {
+  const handleGamePageLink = () => {
+    if (selectedGame) {
+      const cleanedGameName = selectedGame.trim(); // Trim any leading/trailing spaces
+      const encodedGameName = encodeURIComponent(cleanedGameName);
+
+      return `/games/${encodedGameName}`;
+    }
+    return '#'; // Return a placeholder if no game is selected
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
-      {/* Home page content */}
-      <GameDropdown gameTitles={gameTitles} />
+      <nav className="navbar">
+        <h1>CrossConnect</h1>
+        <div className="search-bar">
+          <SearchBar onSelectGame={handleSelectGame} gameTitles={gameTitles} darkMode={darkMode} />
+        </div>
+        <button className="dark-mode-button" onClick={handleToggleDarkMode}>
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+      </nav>
+      <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+        <div className="App">
+          <h1>Game Selection</h1>
+          <GameDropdown onSelectGame={handleSelectGame} selectedGame={selectedGame} />
+          {selectedGame && (
+            <div>
+              <button className="game-page-button" disabled={!selectedGame}>
+                <Link to={handleGamePageLink()}>Go to Game Page</Link>
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
